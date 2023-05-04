f61fe3eb57f0c7030b64113a789d4fc844c3ab42
anikhumaryan <111751009+anikhumaryan@users.noreply.github.com>
2023-05-04 18:34:47 +0400

    Create bigfactorial.js

---
+++ bigfactorial.js
@@ -0,0 +1,14 @@
+var n = 747
+function factorial(n) {
+    if (n < 0n) {
+        return "undefined"
+    }else if (n == 0n , n == 1n) {
+        return 1n
+    } else {
+        return n * factorial(n-1n)
+    }
+  }
+
+let result = factorial(BigInt(n))
+
+console.log("fact of "+n+" is " + result)
